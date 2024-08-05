ac83eb0b50f0d422748fabbf0b2f454c1caddcc9
HariPrakash-03 <hariprakash.r.official@gmail.com>
2024-08-05 21:47:11 +0530

    Create Peak_Element.java
    This program is to find the peak elements in a given array, if the adjacent element(if available) of the array is greater than or equal that element is refers to the Peak Element

---
+++ Peak_Element.java
@@ -0,0 +1,32 @@
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+      if(n==1)
+      {
+          return 0;
+      }
+      else if(arr[0]>=arr[1])
+      {
+          return 0;
+      }
+      else if(arr[n-1]>=arr[n-2])
+      {
+          return n-1;
+      }
+      else
+      {
+          for(int i=1;i<=n-2;i++)
+          {
+              if(arr[i]>=arr[i+1] && arr[i]>=arr[i-1])
+              {
+                  return i;
+              }
+          }
+      }
+      return 0;
+    }
+}
